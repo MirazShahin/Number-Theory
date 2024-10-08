5b4502a9d6381ab75c8e22a3cabd0e7fb8173ff8
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-10-08 20:48:05 +0600

    Update Sieve.cpp

--- Sieve of Eratosthenes/Sieve.cpp
+++ Sieve of Eratosthenes/Sieve.cpp
@@ -1,23 +1,25 @@
-const int MX = 1e6; 
-vector<int> primes;
-bitset<MX + 5> mark; 
- 
+const int N = 1e6;
+bitset < N + 5 > mark;
+vector<ll>primes;
 void sieve() {
-    int limit = sqrt(MX);
-    for (int i = 3; i <= limit; i += 2) {
-        if (!mark[i] and (i * i) <= MX) {
-            for (int j = i * i; j <= MX; j += (i << 1)) {
-                mark[j] = 1;
+    int sq = sqrt(N);
+    for (int i = 4; i <= N; i += 2) {
+        mark[i] = true;
+    }
+    for (int i = 3; i <= sq; i += 2) {
+        if (!mark[i]) {
+            for (int j = i * i; j <= N; j += (i << 1)) {
+                mark[j] = true;
             }
         }
     }
     primes.push_back(2);
-    for (int i = 3; i <= MX; i += 2) {
-        if (!mark[i]) primes.push_back(i);
+    for (int i = 3; i <= N; i += 2) {
+        if (!mark[i])
+            primes.push_back(i);
     }
 }
-
-bool isPrime(ll n) {
-    if(n < 2) return false;
+bool isPrime(int n) {
+    if (n < 2) return false;
     return !mark[n];
-} 
+}
